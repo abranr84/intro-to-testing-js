93cf3de893965e1d0a675541575befa6b382793f
Abran Rodriguez Jr <abranrodriguezjr@gmail.com>
2022-08-24 15:09:47 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// function helloWorld() {
+//     return ("Hello, World!")
+// }
+
+const helloWorld = function() {
+    return "Hello, World!"
+}
+
+function sayHello(){
+
 }
